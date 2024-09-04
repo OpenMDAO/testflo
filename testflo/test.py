f05cb3dccb139d2d7f9023df3c85787345eb3d44
Bret Naylor <naylor.b@gmail.com>
2024-09-04 14:21:47 -0400

    removed separate calls to setUp and tearDown

--- testflo/test.py
+++ testflo/test.py
@@ -319,11 +319,11 @@ class Test(object):
                             status, expected2 = _try_call(getattr(parent, funcname))
                         self.err_msg = errstream.getvalue()
                     else: # use unittest code to run the test and handle subtests
-                        if tcase_setup:
-                            status, expected = _try_call(tcase_setup)
-                            if status != 'OK':
-                                done = True
-                                tcase_teardown = None
+                        # if tcase_setup:
+                        #     status, expected = _try_call(tcase_setup)
+                        #     if status != 'OK':
+                        #         done = True
+                        #         # tcase_teardown = None
 
                         result= UnitTestResult()
                         parent.run(result)
@@ -353,8 +353,8 @@ class Test(object):
                     self.memory_usage = get_memory_usage()
                     self.expected_fail = expected or expected2 or expected3
 
-                    if tcase_teardown:
-                        _try_call(tcase_teardown)
+                    # if tcase_teardown:
+                    #     _try_call(tcase_teardown)
 
                     if mod_teardown:
                         _try_call(mod_teardown)
